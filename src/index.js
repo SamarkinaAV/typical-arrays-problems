6e99e8ea1affef4899728ed55e546ec42887a1d0
SamarkinaAV <opal-nii@yandex.ru>
2020-02-20 19:39:51 +0400

    init solution task:

--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
-
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+    if (typeof array !== 'undefined' && array.length > 0) {
+        array.sort((a, b) => a - b);
+        return array[0];
+    } else {
+        return 0;
+    }
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    if (typeof array !== 'undefined' && array.length > 0) {
+        array.sort((a, b) => b - a);
+        return array[0];
+    } else {
+        return 0;
+    }
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    var sum = 0;
+    if (typeof array !== 'undefined' && array.length > 0) {
+        for (var i = 0; i < array.length; i++) {
+            sum = sum + array[i];
+        }
+        return (sum / array.length);
+    } else {
+        return 0;
+    }
 }
